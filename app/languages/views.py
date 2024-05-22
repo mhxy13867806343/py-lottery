7ed406ea39651f5346bbc55fc5f71bda79a312f0
???? <mhxy13867806343@users.noreply.github.com>
2024-05-22 17:58:49 +0800

    update

--- app/languages/views.py
+++ app/languages/views.py
@@ -9,14 +9,14 @@ from tool.dbThrottling import limiter
 from pydantic import BaseModel
 
 languagesApp = APIRouter(
-    prefix="/v1/h5/outer",
+    prefix="/v1/h5/languages",
     tags=["语言辅助管理"]
 )
 
 
 
 
-@languagesApp.get("/q",description="获取语言辅助管理列表",summary="获取语言辅助管理列表")
+@languagesApp.get("/search",description="获取语言辅助管理列表",summary="获取语言辅助管理列表")
 def getLanguages(q:str="",db: Session = Depends(getDbSession)):
     # 假设 getListAll 函数接收一个年份参数，返回该年份的节假日信息列表
    return httpStatus(data=getListAll(db=db,q=q), message="获取成功", code=status.HTTP_200_OK)
